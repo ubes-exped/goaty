7ff4cf2e9f70879c790bd118578ca3896ecd7e7c
Charlie Harding <charlie_harding@icloud.com>
2020-03-25 00:12:10 +0000

    Document example config

--- example.config.js
+++ example.config.js
@@ -1,9 +1,9 @@
 export default {
-  discordToken: 'x',
-  discordPrefix: '!',
-  masterChannel: 'goatymaster',
+  discordToken: 'x', // The token for your bot
+  discordPrefix: '!', // The prefix used to summon goaty
+  masterChannel: 'goatymaster', // The channel used to control movement
   allowedGuilds: [], // <---- DON'T CHANGE THIS
   blockedVoiceChannels: [], // <---- DON'T CHANGE THIS
-  rabbitMQConnection: 'amqp://localhost',
-  aliases: {},
+  rabbitMQConnection: 'amqp://localhost', // The location of the queue
+  aliases: {}, // Shorthand for specific commands
 };
